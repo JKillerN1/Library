aea0e1b13adec300a9662f8bc6f130cc90940270
Arina <arina2004popova@yandex.ru>
2022-05-15 14:22:31 +0300

    download book covers.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import os
 import lxml
 from urllib.error import HTTPError
+from urllib.parse import urljoin
 
 
 def check_for_redirect(response):
@@ -11,41 +12,60 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
-def download_txt(response, filename, folder='books/'):
-    file_name, avtor_name = filename
+def download_book(response, filename, folder='books/'):
+    file_name = filename
     filename_book = os.path.join(folder, f'{file_name}.txt')
+    with open(filename_book, 'w', encoding="utf-8") as file:
+        file.write(response.text)
 
-    with open(filename_book, 'wb') as file:
+
+def download_picture(response, filename, folder='images/'):
+    download_url = 'https://tululu.org'
+    response = requests.get(urljoin(download_url, filename))
+    picture_name=filename.split('/')[2]
+    path = os.path.join(folder, picture_name)
+    with open(path, 'wb') as file:
         file.write(response.content)
 
 
-def filename_f(book_url):
+def parse_book_page(id, book_url):
+    response = requests.get(book_url.format(id=id))
+    response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
+
     title_tag = soup.find('h1').text
-    # print(title_tag)
     title_book, title_avtor = title_tag.split(' :: ')
     title_book = title_book.split()
     title_book = ' '.join(title_book)
+
+    title_tag = soup.find('div', class_='bookimage').find('img')['src']
+
+    download_book(response, title_book, folder='books/')
+    download_picture(response, title_tag, folder='images/')
     return title_book, title_avtor
 
 
 if __name__ == "__main__":
-    os.makedirs("books", exist_ok=True)
-    url = "https://tululu.org/txt.php"
-    # download = 'https://tululu.org'
+    os.makedirs("books", exist_ok = True)
+    os.makedirs("images", exist_ok = True)
+    url_book = "https://tululu.org/txt.php"
+    download_url = 'https://tululu.org'
+    book_url = 'https://tululu.org/b{id}/'
+
     for book_num in range(1, 11):
         params = {"id": book_num}
+        response = requests.get(url_book, params)
+        response_p=requests.get(book_url)
+
         try:
-            book_url = f'https://tululu.org/b{book_num}/'
-            response = requests.get(book_url)
             response.raise_for_status()
+            #response_p.raise_for_status()
             check_for_redirect(response)
-            # response.raise_for_status()
-            # book_url = book_url.format(book_num)
-            book_page = filename_f(response)
-            # print(book_page)
-            response2 = requests.get(url, params=params)
-            check_for_redirect(response2)
-            download_txt(response2, book_page)
-        except requests.HTTPError:
+            parse_book_page(book_num, book_url)
+
+            #download_picture(book_num, book_url)
+            #check_for_redirect(response2)
+
+            #print(urljoin(download, picture(response)))
+        except requests.exceptions.HTTPError:
             print("такой книги не существует")
