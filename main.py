28756aa64153b1cee3958c995dbe876af4ed002f
Arina <arina2004popova@yandex.ru>
2022-07-13 11:51:43 +0300

    image downloading is fixed

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def dowloand_comments(response):
     comments = soup.find_all('div', class_='texts')
     for comment in comments:
         com = comment.find('span', class_='black').text
-        print(com)
+        return com
 
 
 def download_book(response, id, filename, folder='books/'):
@@ -25,17 +25,17 @@ def download_book(response, id, filename, folder='books/'):
         file.write(response.text)
 
 
-def download_picture(filename, folder='images/'):
+def download_picture(title_tag, filename, folder='images/'):
+    
     download_url = 'https://tululu.org'
     response = requests.get(urljoin(download_url, filename))
-    picture_name = filename  # .split('/')[2]
+    picture_name = title_tag.split('/')[2]
     path = os.path.join(folder, picture_name)
     with open(path, 'wb') as file:
         file.write(response.content)
 
 
-def parse_book_page(response):
-    soup = BeautifulSoup(response.text, 'lxml')
+def parse_book_page(soup):
 
     title_tag = soup.find('h1').text
     title_book, title_avtor = title_tag.split(' :: ')
@@ -46,11 +46,6 @@ def parse_book_page(response):
 
     genres = soup.find_all('span', class_='d_book')
     genre_book = [genre.find('a').text for genre in genres]
-    # for genre in genres:
-    #   genre_book = genre.find('a').text
-    # print('Жанр:', " ".join(genre_book))
-
-    #title_tag = soup.find('div', class_='bookimage').find('img')['src']
 
     return title_book, title_avtor, " ".join(genre_book)
 
@@ -68,7 +63,6 @@ if __name__ == "__main__":
     os.makedirs("images", exist_ok=True)
 
     main_page_url = "https://tululu.org/txt.php"
-    download_url = 'https://tululu.org'
     book_page_url = 'https://tululu.org/b{id}/'
 
     for book_num in range(start_id, end_id):
@@ -77,19 +71,20 @@ if __name__ == "__main__":
         response_page = requests.get(book_page_url.format(id=params['id']))
 
         try:
+            soup = BeautifulSoup(response_page.text, 'lxml')
             response.raise_for_status()
             response_page.raise_for_status()
             check_for_redirect(response)
-            print(parse_book_page(response_page))
-            print(dowloand_comments(response_page))
+            parse_book_page(soup)
+            dowloand_comments(response_page)
 
-            if parse_book_page(response_page):
-                download_book(response, book_num, parse_book_page(response_page)[0], folder='books/')
-                download_picture(parse_book_page(response_page)[0], folder='images/')
+            if parse_book_page(soup):
+                tag = soup.find('div', class_='bookimage').find('img')['src']
+                download_book(response, book_num, parse_book_page(soup)[0], folder='books/')
+                download_picture(tag, parse_book_page(soup)[0], folder='images/')
 
         except requests.exceptions.HTTPError:
             print('такой книги не существует')
 
-        except ConnectionResetError:
+        except requests.exceptions.ConnectionError:
             print('прервано соединение')
-
